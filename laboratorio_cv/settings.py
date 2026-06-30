f53e699d133467f9732752902af39831a45f3463
egomex <egomezl5@unemi.edu.ec>
2026-06-29 20:18:28 -0500

    Funcional

--- laboratorio_cv/settings.py
+++ laboratorio_cv/settings.py
@@ -57,10 +57,20 @@ CHANNEL_LAYERS = {
     },
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'laboratorio_cv',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
